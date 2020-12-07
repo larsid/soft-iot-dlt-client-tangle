7db58e36421b6a780184515bf139bd1ba3905668
Uellington Damasceno <uellington99@gmail.com>
2020-12-07 10:31:25 -0300

    Abstract service class removed.

--- src/main/java/dlt/client/tangle/model/LedgerReader.java
+++ src/main/java/dlt/client/tangle/model/LedgerReader.java
@@ -7,6 +7,13 @@ import dlt.client.tangle.services.ILedgerReader;
  * @author Uellington Damasceno
  * @version 0.0.1
  */
-public class LedgerReader extends Service implements ILedgerReader{
-    
+public class LedgerReader implements ILedgerReader {
+
+    public void start() {
+        System.out.println("LedgerReader started");
+    }
+
+    public void stop() {
+        System.out.println("LedgerReader stopped");
+    }
 }
--- src/main/java/dlt/client/tangle/model/LedgerWriter.java
+++ src/main/java/dlt/client/tangle/model/LedgerWriter.java
@@ -7,6 +7,13 @@ import dlt.client.tangle.services.ILedgerWriter;
  * @author Uellington Damasceno
  * @version 0.0.1
  */
-public class LedgerWriter extends Service implements ILedgerWriter{
-    
+public class LedgerWriter implements ILedgerWriter {
+
+    public void start() {
+        System.out.println("LedgerWriter started");
+    }
+
+    public void stop() {
+        System.out.println("LedgerWriter stopped");
+    }
 }
